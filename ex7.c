0a51eda88c9fb3ee45a97c717830b16d9e0a9853
Bruno Braga <braga.bruno81@gmail.com>
2022-09-08 12:44:31 -0500

    added double variables

--- ex7.c
+++ ex7.c
@@ -23,7 +23,13 @@ int main(int argc, char*argv[])
     printf("You have %d bugs at the imaginary rate af %f.\n", bugs, bug_rate);
 
     long universe_of_defects = 1L * 1024L * 1024L * 1024L;
-    printf("The entire universe has %ld bugs.\n", part_of_universe);
+    printf("The entire universe has %ld bugs.\n", universe_of_defects);
+
+    double expected_bugs = bugs * bug_rate;
+    printf("You are expected to have %f bugs.\n", expected_bugs);
+
+    double parts_of_universe = expected_bugs/ universe_of_defects;
+    printf("That isonly a %e portion of the universe.\n", parts_of_universe);
 
     //this makes no sense, just a demo of something weird
     char nul_byte = '\0';
